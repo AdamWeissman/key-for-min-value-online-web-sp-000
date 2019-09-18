f7c0c54d8a76462c257fd2d469fc3435487de74e
Adam Weissman <adam@comedytragedyepic.com>
2019-09-18 11:28:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
   new = name_hash.to_a
+  compare_to_me = new[0]
+  counter = 0
+  until counter == new.length
+    new.each do |i|
+      if i[1] < compare_to_me[1]
+        compare_to_me = i
+        counter += 1
+      else
+        counter += 1
+      end
+    end
+  end
+  return compare_to_me[0]
 end
